8376ed54285950ff77671cc83373d4300f93aa28
ovaconsultants <hr@ova2consultancy.com>
2024-12-14 21:04:02 -0500

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,5 @@
-export const siteUrl = 'https://ova2consultants.com';
-export const generateRobotsTxt = true;
-export const sitemapSize = 7000;
+module.exports = {
+  siteUrl: 'https://ova2consultants.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   images: {
     unoptimized: true, // Disable Image Optimization for static export
   },
-  experimental: {
-    ppr: 'incremental',
-  },
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
